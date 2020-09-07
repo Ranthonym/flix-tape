ebed0bedce43b5a0b68bd28a8ba859deac0506b9
mithrax <rahul.anthonym@gmail.com>
2020-09-07 13:44:27 -0400

    add useEffect fetch requests to api in parent app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Search from "./components/Search/Search";
+import Movie from "./components/Movie/Movie";
 
 const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fd78d98e";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setMovies(jsonResponse.Search);
+      });
+  }, []);
+
+  const search = (searchValue) => {
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setMovies(jsonResponse.Search);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">Welcome to Flix Tape!</header>
-      <Search />
+      <Search search={search} />
     </div>
   );
 }
