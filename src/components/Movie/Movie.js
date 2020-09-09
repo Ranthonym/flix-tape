90857794b8218f312cc45c03cd838d7a2dc64190
mithrax <rahul.anthonym@gmail.com>
2020-09-09 15:34:54 -0400

    minor change in movie prop object

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -5,24 +5,23 @@ import classes from "./Movie.module.css";
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-const Movie = (movie) => {
+const Movie = (props) => {
+  const movie = props.movie;
   const poster =
-    movie.movie.Poster === "N/A"
-      ? DEFAULT_PLACEHOLDER_IMAGE
-      : movie.movie.Poster;
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
 
   // console.log(movie);
   return (
     <div className={classes.Movie}>
-      <h2>{movie.movie.Title}</h2>
+      <h2>{movie.Title}</h2>
       <div>
         <img
           width="200"
-          alt={`The movie titled: ${movie.movie.Title}`}
+          alt={`The movie titled: ${movie.Title}`}
           src={poster}
         />
       </div>
-      <p>({movie.movie.Year})</p>
+      <p>({movie.Year})</p>
     </div>
   );
 };
